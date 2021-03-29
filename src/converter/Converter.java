74c2cfa4f9741f61d7e76ab7fcfdedc039918231
yahe0005 <yahe0005@stud.hs-kl.de>
2021-03-29 17:20:41 +0200

    update project structure

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -12,7 +12,7 @@ import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
 /**
- * Konverter für EKG Daten vom EDF Format ins WFDB Format
+ * Konverter für EKG Daten vom EDF Format ins WFDB Format.
  */
 public class Converter {
     private EDFreader EDFFile;
@@ -23,11 +23,12 @@ public class Converter {
     private int numberOfSignals;
     private long numberOfSignalsPerSample;
 
-    int[] initialvalues;
-    int[] checksums;
+    private int[] initialValues;
+    private int[] checksums;
+
 
     /**
-     * Erzeugt die Dateien in die später beschrieben werden (.dat Datei und .hea Datei).
+     * Erzeugt die Dateien in die später geschrieben werden soll (.dat Datei und .hea Datei).
      * Liest die gegebene EDF Datei ein.
      *
      * @param inputFileName Name der Eingabedatei
@@ -43,16 +44,18 @@ public class Converter {
 
     }
 
+
     /**
-     * Konvertiert die gegebene EDF Datei ins WFDB Format
+     * Konvertiert die gegebene EDF Datei ins WFDB Format.
      *
      * @throws IOException
      * @throws EDFException
      */
     public void convert() throws IOException, EDFException {
         getNonSignalSpecificInformation();
-        initialvalues = new int[numberOfSignals];
+        initialValues = new int[numberOfSignals];
         checksums = new int[numberOfSignals];
+
         createSignalFile();
         for (int i = 0; i < numberOfSignals; i++) {
             getSignalSpecificInformation(i);
@@ -63,8 +66,9 @@ public class Converter {
         signal.close();
     }
 
+
     /**
-     * Entnimmt der eingelesenen EDF Datei alle wichtigen, nicht signalspizifischen, Informationen und schreibt diese in die Header Datei im WFDB Format
+     * Entnimmt der eingelesenen EDF Datei alle wichtigen, nicht signalspizifischen, Informationen und schreibt diese in die Header Datei im WFDB Format.
      *
      * @throws EDFException
      * @throws IOException
@@ -75,14 +79,18 @@ public class Converter {
         double samplingFrequency = EDFFile.getSampleFrequency(0);
         numberOfSignalsPerSample = EDFFile.getTotalSamples(0);
 
-        String baseTime = EDFFile.getStartTimeHour() + ":" +
+        String baseTime =
+                EDFFile.getStartTimeHour() + ":" +
                 EDFFile.getStartTimeMinute() + ":" +
                 EDFFile.getStartTimeSecond();
-        String baseDate = EDFFile.getStartDateDay() + "/" +
+
+        String baseDate =
+                EDFFile.getStartDateDay() + "/" +
                 EDFFile.getStartDateMonth() + "/" +
                 EDFFile.getStartDateYear();
 
-        String firstLine = recordName.trim() + " " +
+        String firstLine =
+                recordName.trim() + " " +
                 numberOfSignals + " " +
                 samplingFrequency + " " +
                 numberOfSignalsPerSample + " " +
@@ -92,8 +100,9 @@ public class Converter {
         headerWriter.write(firstLine);
     }
 
+
     /**
-     * Entnimmt der EDF Datei alle signalspezifischen Informationen und schreibt diese in die Header Datei im WFDB Format
+     * Entnimmt der EDF Datei alle signalspezifischen Informationen und schreibt diese in die Header Datei im WFDB Format.
      *
      * @param signalNumber Nummer des Signals
      * @throws EDFException
@@ -103,21 +112,20 @@ public class Converter {
         String fileName = recordName.trim() + ".dat";
         int digitalMaximum = EDFFile.getDigitalMaximum(signalNumber);
         int format = Integer.toBinaryString(digitalMaximum).length() + 1;
-        double Gain = (EDFFile.getPhysicalMaximum(signalNumber) - EDFFile.getPhysicalMinimum(signalNumber)) /
+        double gain = (EDFFile.getPhysicalMaximum(signalNumber) - EDFFile.getPhysicalMinimum(signalNumber)) /
                 (EDFFile.getDigitalMaximum(signalNumber) - EDFFile.getDigitalMinimum(signalNumber));
-        double ADCGain = 1.0 / Gain;
+        double ADCGain = 1.0 / gain;
         String baseline = "(0)";
         String unit = "/" + EDFFile.getPhysicalDimension(signalNumber).trim();
         int ADCResolution = Integer.toBinaryString(digitalMaximum).length() + 1;
         int ADCZero = 0;
-        int[] buf2 = new int[1];
-        EDFFile.readDigitalSamples(signalNumber, buf2);
-        int initialValue = initialvalues[signalNumber];
+        int initialValue = initialValues[signalNumber];
         int checksum = checksums[signalNumber];
-        int blocksize = 0;
+        int blockSize = 0;
         String description = EDFFile.getSignalLabel(signalNumber);
 
-        String output = fileName + " " +
+        String output =
+                fileName + " " +
                 format + " " +
                 ADCGain +
                 baseline +
@@ -126,38 +134,44 @@ public class Converter {
                 ADCZero + " " +
                 initialValue + " " +
                 checksum + " " +
-                blocksize + " " +
+                blockSize + " " +
                 description + "\n";
 
         headerWriter.write(output);
     }
 
+
     /**
-     * entnimmt der EDF Datei alle Signalpegel und schreibt diese in die .dat Datei im WFDB Format
+     * entnimmt der EDF Datei alle Signalpegel und schreibt diese in die .dat Datei im WFDB Format.
      *
      * @throws IOException
      * @throws EDFException
      */
     private void createSignalFile() throws IOException, EDFException {
-        int[] totalbuffer = new int[(int) (numberOfSignalsPerSample * numberOfSignals)];
+        int[] totalBuffer = new int[(int) (numberOfSignalsPerSample * numberOfSignals)];
+
         for (int i = 0; i < numberOfSignals; i++) {
-            int[] buf = new int[(int) numberOfSignalsPerSample];
-            EDFFile.readDigitalSamples(i, buf);
-            checksums[i] = calcChecksum(buf);
-            initialvalues[i] = buf[0];
-            for (int j = 0; j < buf.length; j++) {
-                totalbuffer[(j * 12) + i] = buf[j];
+            int[] buffer = new int[(int) numberOfSignalsPerSample];
+            EDFFile.readDigitalSamples(i, buffer);
+            checksums[i] = calculateChecksum(buffer);
+            initialValues[i] = buffer[0];
+
+            for (int j = 0; j < buffer.length; j++) {
+                totalBuffer[(j * 12) + i] = buffer[j];
             }
+
         }
 
-        byte[] outputBuffer = new byte[totalbuffer.length * 2];
-        for (int i = 0; i < totalbuffer.length; i++) {
-            outputBuffer[i * 2] = (byte) (totalbuffer[i] & 0xff);
-            outputBuffer[(i * 2) + 1] = (byte) ((totalbuffer[i] >> 8) & 0xff);
+        byte[] outputBuffer = new byte[totalBuffer.length * 2];
+
+        for (int i = 0; i < totalBuffer.length; i++) {
+            outputBuffer[i * 2] = (byte) (totalBuffer[i] & 0xff);
+            outputBuffer[(i * 2) + 1] = (byte) ((totalBuffer[i] >> 8) & 0xff);
         }
 
         signal.write(outputBuffer, 0, outputBuffer.length);
     }
+
 
     /**
      * Liest die EDF Datei mithilfe der Library ein.
@@ -170,30 +184,35 @@ public class Converter {
         EDFFile = new EDFreader(fileName);
     }
 
+
     /**
      * Berechnet die 16-bit Prüfsumme für ein Signal und gibt diese zurück.
      *
      * @param buf Array mit allen Amplitudenwerten des Signals
      * @return Prüfsumme als Integer
      */
-    private int calcChecksum(int[] buf) {
-        Checksum t = new CRC32();
+    private int calculateChecksum(int[] buf) {
+        Checksum checksum = new CRC32();
+
         for (int item : buf) {
-            t.update(item);
+            checksum.update(item);
         }
-        String value = Long.toBinaryString(t.getValue());
+
+        String value = Long.toBinaryString(checksum.getValue());
 
         if (value.length() < 32) {
             int numberOfZero = 32 - value.length();
-            StringBuilder finalValue = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
+
             for (int i = 0; i < numberOfZero; i++) {
-                finalValue.append(0);
+                stringBuilder.append(0);
             }
-            finalValue.append(value);
-            return Integer.parseInt(finalValue.substring(16, 32), 2);
-        } else {
-            return Integer.parseInt(value.substring(16, 32), 2);
+
+            stringBuilder.append(value);
+            return Integer.parseInt(stringBuilder.substring(16, 32), 2);
         }
+
+        return Integer.parseInt(value.substring(16, 32), 2);
 
     }
 
